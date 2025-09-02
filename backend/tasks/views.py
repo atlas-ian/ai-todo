6a69535de345a906e78207daaca5530df735edbb
atlas-ian <adarshn54688@gmail.com>
2025-09-02 17:23:56 +0530

    Add views and urls for tasks app, update main app urls, setup initial health check route with DB status

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def health_check(views):
+    return Response({
+        'status': 'healthy',
+        'message': 'Smart ToDo API is running!',
+        'database': 'connected'
+    })
